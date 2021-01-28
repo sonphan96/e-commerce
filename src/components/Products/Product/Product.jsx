b454c41bb7ce37f14f1b3a0945a6de054cd7eca8
SonPhan <phantruongson96@gmail.com>
2021-01-28 17:43:06 -0500

    Set up Layout

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -4,27 +4,36 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from
 import { AddShoppingCart} from '@material-ui/icons';
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
     const classes = useStyles();
-
+    
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+
             <CardContent>
                 <div className={classes.cardContent}>
+
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
+
                     <Typography variant="h5" >
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary"> 
-                        {product.description}
-                    </Typography>
+
                 </div>
+
+                <Typography 
+                        dangerouslySetInnerHTML={{ __html: product.description }} 
+                        variant="body2" 
+                        color="textSecondary" 
+                    /> 
+                    
             </CardContent>
+
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)} >
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>
