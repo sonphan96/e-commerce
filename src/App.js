30f2c81df9f54120756c51802d494bba942df549
SonPhan <phantruongson96@gmail.com>
2021-01-28 18:47:57 -0500

    Added Router and Cart Button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [ products, setProducts ] = useState([]);
@@ -21,20 +22,49 @@ const App = () => {
     }, [])
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quantity);
+
+        setCart(cart);
     }
 
-    
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+
+        setCart(cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart);
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+
+        setCart(cart);
+    }
 
     return (
-        <div>
-            <Navbar totalItems={cart.total_items} />
-            <Products products={products} onAddToCart={handleAddToCart} />
-            
-            <Cart cart={cart} />
-        </div>
-        
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items} />
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart} />  
+                    </Route>
+
+                    <Route exact path="/cart">
+                        <Cart 
+                            cart={cart} 
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
+                    </Route>       
+                </Switch>
+            </div>
+        </Router>
     );
 };
 
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
         <Typography variant="subtitle1">
-            You have no item in your shopping cart, start adding some!
+            You have no item in your shopping cart, 
+            <Link to="/" className={classes.link}> start adding some! </Link> 
         </Typography>
     );
 
@@ -18,7 +20,10 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id} >
-                        <CartItem item={item} />
+                        <CartItem item={item} 
+                            onUpdateCartQty={handleUpdateCartQty} 
+                            onRemoveFromCart={handleRemoveFromCart}
+                        />
                     </Grid>
                 ))}
             </Grid>
@@ -32,7 +37,9 @@ const Cart = ({ cart }) => {
                         size="large" 
                         type="button" 
                         variant="contained" 
-                        color="secondary" >Empty Cart</Button>
+                        color="secondary" 
+                        onClick={handleEmptyCart}
+                    >Empty Cart</Button>
                     <Button 
                         className={classes.checkoutButton} 
                         size="large" 
